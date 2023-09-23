e5b27e22aba25fad0fdcad2f18822431d1842903
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-09-22 22:41:53 -0300

    feat: create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,42 @@
+import { FiLogOut } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import { auth } from "../../services/firebaseConnection";
+import { signOut } from "firebase/auth";
+import toast from "react-hot-toast";
+
+const hoverClasses =
+  "flex items-center gap-1 px-2 py-1 rounded-full transition-colors duration-300 ease-in-out hover:bg-orangeLinkerHover";
+
+const buttonClasses =
+  "flex items-center justify-center gap-2 px-2 py-1 rounded-full transition-colors duration-300 ease-in-out hover:bg-orangeLinkerHover";
+
+export function Header() {
+  async function handleLogout() {
+    await signOut(auth);
+    toast.success("Logout realizado com sucesso! \n Até a próxima 😁")
+  }
+
+  return (
+    <header className="w-full max-w-2xl mt-4 px-2">
+      <nav className="w-full bg-orangeLinker h-12 flex items-center justify-around rounded-md font-medium">
+        <div className="flex gap-4">
+          <Link to="/" className={hoverClasses}>
+            Home
+          </Link>
+          <Link to="/admin" className={hoverClasses}>
+            Links
+          </Link>
+          <Link to="/admin/social" className={hoverClasses}>
+            Social
+          </Link>
+        </div>
+
+        <button className={buttonClasses} onClick={handleLogout}>
+          <span className="hidden md:block">Logout</span>{" "}
+          <FiLogOut size={24} color="#fff" />
+        </button>
+      </nav>
+    </header>
+  );
+}
